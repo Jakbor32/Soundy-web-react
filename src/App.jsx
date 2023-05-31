f68a363cdfec14d139f163b7a6f7494b112f5410
Jakub Borowy <borowyjakub32@gmail.com>
2023-05-31 23:47:11 +0200

    Add Helmet for meta tag control and set favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import favicon16x16 from "./assets/favicon-16x16.png";
+import faviconICO from "./assets/favicon.ico";
+
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
     <>
+      <Helmet>
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
+        <link rel="icon" type="image/x-icon" href={faviconICO} />
+      </Helmet>
       <div className="text-2xl">Hello world!</div>
     </>
   );
