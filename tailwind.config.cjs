5cdb83297e2f6c684921ee506325118659b29fb2
Jakub Borowy <borowyjakub32@gmail.com>
2023-06-10 23:56:58 +0200

    Adding react-icons, fix colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,22 +5,24 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                hvrNavClr: "#CCA873",
+                plusArrowClr: "#4B96AC",
+                redBarClr: "#5B0000",
+                featuredBgClr: "#0D0B0B",
+                featuredTitleClr: "#D4B11C",
+                tourBarClr: "#270202",
+                starClr: "#FFB800",
+                promoBgClr: "#0F0303",
+                footerBtnClr: "#1E1C1C",
+                hrClr: "#FFE7E7"
+            }
+        },
         fontFamily: {
             "rubik": ["rubik", "sans-serif"]
         },
-        colors: {
-            hvrNavClr: "#CCA873",
-            plusArrowClr: "#4B96AC",
-            redBarClr: "#5B0000",
-            featuredBgClr: "#0D0B0B",
-            featuredTitleClr: "#D4B11C",
-            tourBarClr: "#270202",
-            starClr: "#FFB800",
-            promoBgClr: "#0F0303",
-            footerBtnClr: "#1E1C1C",
-            hrClr: "FFE7E7"
-        }
+
     },
     plugins: [],
 }
