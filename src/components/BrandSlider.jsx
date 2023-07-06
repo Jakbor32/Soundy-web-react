8750be6da73f65d828f2b6aa45dbcac25cbe4790
Jakub <borowyjakub32@gmail.com>
2023-07-06 22:42:49 +0200

    Code optimization

--- src/components/BrandSlider.jsx
+++ src/components/BrandSlider.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import {spotifyLogo, youtubeLogo, tiktokLogo, appleMusicLogo} from '../lib/Constants'
+import React from "react";
+import { brandLogos } from "../lib/constants";
 
 const BrandSlider = () => {
   return (
-    <section className='bg-brandBarClr relative shadow-md shadow-black'>
-        <div class="absolute left-0 top-0 h-full w-1/2 bg-gradient-to-r from-white opacity-5"></div>
-        <div class="absolute right-0 top-0 h-full w-1/2 bg-gradient-to-l from-white opacity-5"></div>
-    <div className='container mx-auto  flex lg:justify-between md:px-20 px-5 py-5 flex-row flex-wrap items-center justify-center gap-6 lg:gap-0'>
-        <img className='xl:h-16  md:h-12 h-8' src={spotifyLogo} alt="Spotify Logo" />
-        <img className='xl:h-16  md:h-12 h-8' src={youtubeLogo} alt="YouTube Logo" />
-        <img className='xl:h-16  md:h-12 h-8' src={tiktokLogo} alt="TikTok Logo" />
-        <img className='xl:h-16  md:h-12 h-8'  src={appleMusicLogo} alt="Apple Music Logo" />
-    </div>
+    <section className="relative shadow-md bg-brandBarClr shadow-black">
+      <div className="absolute top-0 left-0 w-1/2 h-full bg-gradient-to-r from-white opacity-5"></div>
+      <div className="absolute top-0 right-0 w-1/2 h-full bg-gradient-to-l from-white opacity-5"></div>
+      <div className="container flex flex-row flex-wrap items-center justify-center gap-6 px-5 py-5 mx-auto lg:justify-between md:px-20 lg:gap-0">
+        {brandLogos.map((logo) => (
+          <img
+            key={logo.src}
+            className="h-8 xl:h-16 md:h-12"
+            src={logo.src}
+            alt={logo.alt}
+          />
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default BrandSlider
+export default BrandSlider;
--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -1,39 +1,42 @@
-import React from 'react'
-import Button from './Button'
-import { Spotify } from 'react-spotify-embed'
+import React from "react"
+import Button from "./Button"
+import { Spotify } from "react-spotify-embed";
+import { musicData, musicSectionTitle } from "./constants.jsx";
 
 const MusicSection = () => {
   return (
     <section>
-    <div className="py-20 container mx-auto md:px-20  px-5">
+    <div className="container px-5 py-20 mx-auto md:px-20">
       <div className="flex flex-col items-center gap-8">
-        <h2 className=" text-center font-epilogue text-5xl uppercase text-white">
-        Immerse Yourself in Our Music
+        <h2 className="text-5xl text-center text-white uppercase font-epilogue">
+        {musicSectionTitle}
         </h2>
-        <div className="h-1 w-48 bg-redBarClr"></div>
+        <div className="w-48 h-1 bg-redBarClr"></div>
       </div>
-      <div className="flex flex-col xl:flex-row items-center justify-around gap-5 py-20">
-        <div className="flex flex-col w-full px-3 sm:px-10 xl:px-0 xl:w-1/2 gap-6">
-          <div className='rounded-xl shadow-md shadow-white/20 bg-redBarClr'>
-          <Spotify className='rotate-2 hover:rotate-0 duration-200' wide link="https://open.spotify.com/track/4hJ9GlqWQKgEU24yUbqDZB?si=49595976d03b4aa8"/>
-          </div>
-          <div className='rounded-xl shadow-md shadow-white/20 bg-redBarClr'>
-          <Spotify className='rotate-2 hover:rotate-0 duration-200' wide link="https://open.spotify.com/track/2Bhv2dpgjAfpJejQltrlOJ?si=4c6e80965b6a44e1"/>
-          </div>
-          <div className='rounded-xl shadow-md shadow-white/20 bg-redBarClr'>
-          <Spotify className='rotate-2 hover:rotate-0 duration-200' wide link="https://open.spotify.com/track/4mljaiRBQOvFCjcnzxlQ3F?si=d85d357f72b444ae"/>
-          </div>
-          <div className='rounded-xl shadow-md shadow-white/20 bg-redBarClr'>
-          <Spotify className='rotate-2 hover:rotate-0 duration-200' wide link="https://open.spotify.com/track/0vyzWCQaPRwaJPbRv9SB1q?si=20fcd0c799914f97"/>
-          </div>
+      <div className="flex flex-col items-center justify-around gap-5 py-20 xl:flex-row">
+        <div className="flex flex-col w-full gap-6 px-3 sm:px-10 xl:px-0 xl:w-1/2">
+    
+          {musicData.map((trackLink) => (
+            <div
+              key={trackLink}
+              className="shadow-md rounded-xl shadow-white/20 bg-redBarClr"
+            >
+              <Spotify
+                className="duration-200 rotate-2 hover:rotate-0"
+                wide
+                link={trackLink}
+              />
+            </div>
+          ))}
         </div>
-        <div className="flex flex-col md:flex-row gap-8 translate-y-8">
-       <Spotify  link="https://open.spotify.com/track/2RdwrJkx3swffe5ylzvfKR?si=4a50e8fa57e94f9e"/>
-       <Spotify  link="https://open.spotify.com/track/6JRKroQWBkGk44TV4HbSl5?si=2c689ab481354f86"/>
+        <div className="flex flex-col gap-8 translate-y-8 md:flex-row">
+        {musicData.slice(4).map((trackLink) => (
+            <Spotify key={trackLink} link={trackLink} />
+          ))}
         </div>
     
       </div>
-      <div className="flex w-full justify-center uppercase tracking-widest">
+      <div className="flex justify-center w-full tracking-widest uppercase">
         <Button>Listen Further</Button>
       </div>
     </div>
