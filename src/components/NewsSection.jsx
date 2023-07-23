157362d49b5900b8bfe749e7cd6abf86e04e7673
Jakub <borowyjakub32@gmail.com>
2023-07-23 22:09:59 +0200

    Start release/vercel (#8)
    * Update react-router-dom
    
    * Refactor layout classes, minor style tweaks
    
    * Adding page router,pages,scroll

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -7,14 +7,14 @@ const NewsSection = () => {
 
   return (
     <section>
-      <div className="container mx-auto md:px-20 py-20 px-5">
+      <div className="container px-5 py-20 mx-auto md:px-20">
         <div className="flex flex-col items-center gap-8">
-          <h2 className=" text-center font-epilogue text-5xl uppercase text-white">
+          <h2 className="text-5xl text-center text-white uppercase  font-epilogue">
             Featured News
           </h2>
-          <div className="h-1 w-48 bg-redBarClr"></div>
+          <div className="w-48 h-1 bg-redBarClr"></div>
         </div>
-        <div className=" flex-col flex justify-center items-center gap-8 py-10 lg:flex-row">
+        <div className="flex flex-col items-center justify-center gap-8 py-10  lg:flex-row">
         {featuredNews.map((news) => (
             <Card
               key={news.id}
@@ -27,7 +27,7 @@ const NewsSection = () => {
             />
           ))}
         </div>
-        <div className="flex w-full justify-center uppercase tracking-widest">
+        <div className="flex justify-center w-full tracking-widest uppercase">
           <Button>View All</Button>
         </div>
       </div>
