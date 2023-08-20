3cd79a53c8165453327a94b0187e6c1b7504aad6
Jakub <borowyjakub32@gmail.com>
2023-08-21 01:01:57 +0200

    Configure newsletter via email.js

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,22 +4,21 @@ import {
   socialLinks,
   concertMessage,
   newsletterText,
-  newsletterInputPlaceholder,
-  submitButtonLabel,
   copyrightText,
   newsletterTitle,
   footerLinks,
   linksTitle,
 } from "../../lib/Constants";
+import SubscriptionMailer from "../contactform/SubscriptionMailer";
 
 const Footer = () => {
   return (
-    <footer className="bg-featuredFooterBg py-10 text-zinc-500">
-      <div className="container mx-auto px-4 lg:px-20 ">
-        <div className="flex flex-col-reverse items-center  justify-between gap-5 pt-10 lg:flex-row lg:gap-0">
+    <footer className="py-10 bg-featuredFooterBg text-zinc-500">
+      <div className="container px-4 mx-auto lg:px-20 ">
+        <div className="flex flex-col-reverse items-center justify-between gap-5 pt-10 lg:flex-row lg:gap-0">
           <div className="flex flex-col gap-5 lg:items-start">
             <img src={logo[0].path} className="h-16" alt={logo[0].desc} />
-            <p className="pl-3 text-center text-base leading-7 lg:max-w-xs lg:text-left">
+            <p className="pl-3 text-base leading-7 text-center lg:max-w-xs lg:text-left">
               {concertMessage}
             </p>
           </div>
@@ -33,7 +32,7 @@ const Footer = () => {
               </a>
             ))}
           </div>
-          <div className="flex max-w-full flex-col items-center gap-7 lg:pr-3" id="newsletter">
+          <div className="flex flex-col items-center max-w-full gap-7 lg:pr-3" id="newsletter">
             <div className="flex flex-col items-center lg:items-start">
               <h4 className="-ml-2.5 mb-3 border-l-4 border-redBarClr pl-2 pt-2 text-[1.125rem] text-zinc-100">
                 {newsletterTitle}
@@ -42,19 +41,7 @@ const Footer = () => {
                 {newsletterText}
               </p>
             </div>
-            <div class="w-full lg:w-[24rem] lg:px-0 xl:w-[32rem]">
-              <div class="relative flex flex-col items-center gap-5 sm:flex-row sm:gap-0">
-                <input
-                  id="8"
-                  type="text"
-                  placeholder={newsletterInputPlaceholder}
-                  class="relative h-14 w-full border-2 border-zinc-500  bg-featuredFooterBg pl-3 pr-3 font-thin outline-none"
-                />
-                <button class="relative h-12 w-32 border-zinc-500 bg-footerBtnClr text-2xl font-bold  uppercase text-white sm:absolute sm:right-1 sm:border-l-2">
-                  {submitButtonLabel}
-                </button>
-              </div>
-            </div>
+             <SubscriptionMailer />
           </div>
         </div>
         <hr class="mx-3 my-8 h-px border-0 bg-zinc-700" />
---
+++ src/components/contactform/SubscriptionMailer.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import toast, { Toaster } from "react-hot-toast";
+import {
+  newsletterInputPlaceholder,
+  submitButtonLabel,
+} from "../../lib/Constants";
+
+const SubscriptionMailer = () => {
+  const [email, setEmail] = useState("");
+
+  const notifyGood = () => {
+    toast("Thank you for subscribing to our newsletter!", {
+      duration: 3000,
+      position: "top-right",
+      style: {
+        background: "#006400",
+        color: "#fff",
+        fontSize: ".7rem",
+      },
+    });
+  };
+
+  const notifyBad = () => {
+    toast("Failed to subscribe to the newsletter", {
+      duration: 3000,
+      position: "top-right",
+      style: {
+        background: "#8B0000",
+        color: "#fff",
+        fontSize: ".7rem",
+      },
+    });
+  };
+
+  // EmailJS
+
+  const subscriptionSend = (e) => {
+    e.preventDefault();
+    let templateParams = {
+      email: email,
+    };
+    emailjs
+      .send(
+        "service_g9g3o0j",
+        "template_zifbzgh",
+        templateParams,
+        "xm768EdEMyzz9n9xF"
+      )
+      .then(
+        () => {
+          setEmail("");
+          notifyGood();
+        },
+        () => {
+          notifyBad();
+        }
+      );
+  };
+
+  return (
+    <div class="w-full lg:w-[24rem] lg:px-0 xl:w-[32rem]">
+      <form onSubmit={subscriptionSend}>
+        <div class="relative flex flex-col items-center gap-5 sm:flex-row sm:gap-0">
+          <input
+            type="email"
+            name={email}
+            id={email}
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+            required
+            placeholder={newsletterInputPlaceholder}
+            className="relative w-full pl-3 pr-3 font-thin border-2 outline-none h-14 border-zinc-500 bg-featuredFooterBg"
+          />
+          <button
+            type="submit"
+            className="relative w-32 h-12 text-2xl font-bold text-white uppercase border-zinc-500 bg-footerBtnClr sm:absolute sm:right-1 sm:border-l-2"
+          >
+            {submitButtonLabel}
+          </button>
+          <Toaster />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SubscriptionMailer;
